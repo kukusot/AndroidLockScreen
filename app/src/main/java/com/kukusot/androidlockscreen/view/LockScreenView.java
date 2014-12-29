09df52066355370878b4a23c7bb0da2faf8540d2
stecipec <stefangunjov@gmail.com>
2014-12-29 18:19:23 +0100

    Swipe to unlock

--- app/src/main/java/com/kukusot/androidlockscreen/view/LockScreenView.java
+++ app/src/main/java/com/kukusot/androidlockscreen/view/LockScreenView.java
@@ -2,6 +2,9 @@ package com.kukusot.androidlockscreen.view;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.TextView;
@@ -14,6 +17,7 @@ import com.kukusot.androidlockscreen.app.App;
  * Created by Ivan on 12/23/2014.
  */
 public class LockScreenView extends FrameLayout {
+    private GestureDetector mGestureDetector;
 
     public LockScreenView(Context context) {
         super(context);
@@ -34,13 +38,23 @@ public class LockScreenView extends FrameLayout {
 
         setSystemUiVisibility(SYSTEM_UI_FLAG_IMMERSIVE_STICKY | SYSTEM_UI_FLAG_FULLSCREEN | SYSTEM_UI_FLAG_HIDE_NAVIGATION);
         inflate(getContext(), R.layout.view_lock, this);
-        TextView textView = (TextView) findViewById(R.id.unlock);
-        textView.setOnClickListener(new OnClickListener() {
+        Log.e("tag", "context is"+getContext().getClass().getCanonicalName());
+        mGestureDetector = new GestureDetector(getContext(),new GestureDetector.SimpleOnGestureListener(){
             @Override
-            public void onClick(View v) {
-                App.unlock();
+            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+                if (velocityX < -10.0f || velocityX > 10.0f){
+                    App.unlock();
+                }
+                return true;
+
             }
         });
 
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return mGestureDetector.onTouchEvent(event);
     }
 }
